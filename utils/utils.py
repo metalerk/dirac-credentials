37d6e51ff9a5383b155ccbd3354312572aa227a6
Luis Esteban <rodriguezjluis0@gmail.com>
2017-10-08 05:14:36 -0700

    dev3

--- utils/utils.py
+++ utils/utils.py
@@ -18,7 +18,7 @@ class RedisSession:
 
     @property
     def session_is_active(self):
-        return True if bool(self.manager.get("{}:active".format(self.id))) == True else False
+        return True if self.manager.get("{}:active".format(self.id)).decode('iso-8859-1')) != "False" else False
 
     @property
     def get_obj_identifier(self):
