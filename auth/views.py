07546fd2eafdff60d91e19c6f0a16874009f9b23
Luis Esteban <rodriguezjluis0@gmail.com>
2017-10-08 03:03:10 -0700

    fix 4

--- auth/views.py
+++ auth/views.py
@@ -31,9 +31,7 @@ class AuthBackend(MethodView):
 
         if 'active' in session:
             if session['active']:
-
                 return redirect(url_for('dashboard'))
-
 
         auth_code = self.qs.auth.find_one()['code']
         req_code = request.form.get('code')
@@ -41,6 +39,10 @@ class AuthBackend(MethodView):
         if auth_code == req_code:
             session['active'] = True
             return redirect(url_for('dashboard'))
+        else:
+            return redirect('/')
+
+
 
 class Logout(View):
 
